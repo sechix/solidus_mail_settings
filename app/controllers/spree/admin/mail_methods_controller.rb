c8be6f9441139f4fb494bb7403e825a3384fd7e1
Maria Jose <sechix@gmail.com>
2021-04-22 18:50:46 +0200

    Errors depracated in I18n

--- app/controllers/spree/admin/mail_methods_controller.rb
+++ app/controllers/spree/admin/mail_methods_controller.rb
@@ -9,18 +9,18 @@ module Spree
           Spree::Rentals::Config[name] = value
         end
 
-        flash[:success] = Spree.t(:successfully_updated, resource: Spree.t(:mail_method_settings))
+        flash[:success] = I18n.t('spree.successfully_updated', resource: I18n.t('spree.mail_method_settings'))
         redirect_to edit_admin_mail_method_url
       end
 
       def testmail
         if TestMailer.test_email(try_spree_current_user.email).deliver_now
-          flash[:success] = Spree.t('mail_methods.testmail.delivery_success')
+          flash[:success] = I18n.t('spree.mail_methods.testmail.delivery_success')
         else
-          flash[:error] = Spree.t('mail_methods.testmail.delivery_error')
+          flash[:error] = I18n.t('spree.mail_methods.testmail.delivery_error')
         end
       rescue => e
-        flash[:error] = Spree.t('mail_methods.testmail.error', e: e)
+        flash[:error] = I18n.t('spree.mmail_methods.testmail.error', e: e)
       ensure
         redirect_to edit_admin_mail_method_url
       end
